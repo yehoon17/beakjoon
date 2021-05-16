0fb6571a964516484cb513eae4c52b6f280fd5c5
yehoon17 <83195606+yehoon17@users.noreply.github.com>
2021-05-16 15:23:08 +0900

    Update 1724.py

--- 1724.py
+++ 1724.py
@@ -1,8 +1,29 @@
 #https://www.acmicpc.net/problem/1724
-
-def color(i,j,N,M,lines,colored):
+def continuous(x1,y1,line,x2,y2):
     pass
 
+def color(i,j,N,M,lines,colored):
+    area = 1
+    colored[i][j] = True
+    if i > 0:
+        if not colored[i-1][j]:
+            if continuous(i,j,line,i-1,j):
+                area += color(i-1,j,N,M,lines,colored)
+    if i < N-1:
+        if not colored[i+1][j]:
+            if continuous(i,j,line,i+1,j):
+                area += color(i+1,j,N,M,lines,colored)
+    if j > 0:
+        if not colored[i][j-1]:
+            if continuous(i,j,line,i,j-1):
+                area += color(i,j-1,N,M,lines,colored)
+    if j < M-1:
+        if not colored[i][j+1]:
+            if continuous(i,j,line,i,j+1):
+                area += color(i,j+1,N,M,lines,colored)
+    return area
+                
+    
 N, M = map(int,input().split())
 T = int(input())
 lines = [list(map(int, input().split())) for _ in range(T)]
