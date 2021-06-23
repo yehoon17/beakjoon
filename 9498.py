237b9abef674790dc0fe18bb317fcb8bddebadfd
yehoon17 <83195606+yehoon17@users.noreply.github.com>
2021-06-23 09:36:28 +0900

    Create 9498.py

---
+++ 9498.py
@@ -0,0 +1,13 @@
+#https://www.acmicpc.net/problem/9498
+
+a=int(input())
+if a>89:
+    print('A')
+elif a>79:
+    print('B')
+elif a>69:
+    print('C')
+elif a>59:
+    print('D')
+else:
+    print('F')
