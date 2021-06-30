55b201bef5b4bc2014194f2d66ffb4e63328dfaf
yehoon17 <83195606+yehoon17@users.noreply.github.com>
2021-06-30 09:14:02 +0900

    Create 11651.py

---
+++ 11651.py
@@ -0,0 +1,21 @@
+#https://www.acmicpc.net/problem/11651
+
+import sys
+
+def f1(e):
+    return e[0]
+def f2(e):
+    return e[1]
+
+N=int(sys.stdin.readline().strip())
+
+l=[]
+for i in range(N):
+    line=sys.stdin.readline().strip().split()
+    l.append([int(line[0]),int(line[1])])
+    
+l.sort(key=f1)
+l.sort(key=f2)
+    
+for i in range(N):
+    print(*l[i])
